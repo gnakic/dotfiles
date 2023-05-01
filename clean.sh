37def94216860847f01b23c2579d71d9def4518a
Gabriel Nakic <gnakic@proton.me>
2023-05-01 21:11:14 +0200

    remove emacs

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@ set -o allexport
 . ./.env
 set +o allexport
 
-STOW_FOLDERS="git,emacs,vim,zsh,ghc"
+STOW_FOLDERS="git,vim,zsh,ghc"
 
 pushd "$DOTFILES" || exit
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
