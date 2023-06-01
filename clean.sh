ee1c1fa8b6fbf7fe6840bd8a51ee5d907a66b5fb
Gabriel Nakic <gnakic@proton.me>
2023-06-01 21:23:07 +0200

    terminal

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@ set -o allexport
 . ./.env
 set +o allexport
 
-STOW_FOLDERS="git,vim,zsh,ghc"
+STOW_FOLDERS="git,vim,terminal,ghc"
 
 pushd "$DOTFILES" || exit
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
