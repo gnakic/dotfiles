3200eaa4fecfb0905076ce70f9a3c6cfcea49e2b
Gabriel Nakic <15315645+gnakic@users.noreply.github.com>
2024-03-31 01:43:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+function check_stow() {
+    if ! hash "stow" > /dev/null 2>&1; then
+        if [ "$(uname)" == "Darwin" ]; then
+            brew install stow
+        elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+            sudo apt update
+            sudo apt install stow
+        fi
+    fi
+}
+
+check_stow
+
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 STOW_FOLDERS="git,vim,emacs,terminal,ghc"
@@ -5,7 +18,9 @@ STOW_FOLDERS="git,vim,emacs,terminal,ghc"
 pushd "$DOTFILES" || exit
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
-    stow -D "$folder"
-    stow "$folder"
+    stow -D --target "$HOME" "$folder"
+    stow --adopt --target "$HOME" "$folder"
 done
 popd || exit
+
+git reset --hard
